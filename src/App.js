55ec2ae5ecd7eb0c3f573d7d963e0848a5401eb6
pentaquant <pjsmueller@gmail.com>
2017-07-30 20:01:29 -0700

    Transfers files to the remote repo

--- src/App.js
+++ src/App.js
@@ -1,18 +1,60 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import TaskList from './TaskList.js';
+import Date from './Date.js';
+import Avatar from './Avatar.js';
+import AddButton from './AddButton.js';
+
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      tasks: [
+          {
+              'time': '12',
+              'period': 'AM',
+              'activity_title': 'Finish Tutorial Series',
+              'activity_description': '#ReactForNewbies'
+          }, {
+              'time': '9',
+              'period': 'AM',
+              'activity_title': 'Meeting with Team Leads',
+              'activity_description': 'New Project Kickoff'
+          }, {
+              'time': '11',
+              'period': 'AM',
+              'activity_title': 'Call Mom',
+              'activity_description': 'Return her call before she kills me'
+          }, {
+              'time': '3',
+              'period': 'PM',
+              'activity_title': 'Fix Wifey\'s website',
+              'activity_description': 'FB Ads Integration not working'
+          }, {
+              'time': '6',
+              'period': 'PM',
+              'activity_title': 'Do DB Backups',
+              'activity_description': 'Related to upcoming server migration'
+          }
+      ]
+    };
+  }
+  addTask(){
+    var task = {'time': '5', 'period': 'AM', 'activity_title': 'Jogging', 'activity_description': 'Go for a run!'};
+    var tasks = this.state.tasks.concat(task);
+    this.setState({tasks: tasks});
+  }
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div style={{padding: '30px 30px'}}>
+          <Avatar />
+          <br />
+          <Date />
+          <br />
+          <TaskList tasks={this.state.tasks} />
+          <br />
+          <AddButton onClick={this.addTask.bind(this)} />
       </div>
     );
   }
